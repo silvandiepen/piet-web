1fcfa1da1e7f0a28ff65bc3eeb34210ea45f8db2
Sil <sil@matise.nl>
2018-08-17 13:44:54 +0200

    fix the column unit

--- pages/tools/columns.vue
+++ pages/tools/columns.vue
@@ -1,3 +1,11 @@
 <template>
 	<column-example/>
 </template>
+<script>
+import columnExample from "~/components/column-example.vue";
+export default {
+	components: {
+		columnExample
+	}
+};
+</script>
